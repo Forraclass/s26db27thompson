d3b3d0da0e8f698dc3d0c6eb2f580b4281cbc050
Forraclass <S547289@nwmissouri.edu>
2026-04-10 13:57:17 -0500

    Tristen, Implement update

--- controllers/fruits.js
+++ controllers/fruits.js
@@ -31,7 +31,7 @@ exports.fruit_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"fruit_type":"goat", "cost":12, "size":"large"}
     document.name = req.body.name;
     document.color = req.body.color;
     document.marketPrice = req.body.marketPrice;
@@ -52,6 +52,23 @@ res.send('NOT IMPLEMENTED: Fruit delete DELETE ' + req.params.id);
 };
 
 // Handle Fruit update form on PUT.
-exports.fruit_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Fruit update PUT' + req.params.id);
+exports.fruit_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Fruit.findById( req.params.id)
+        // Do updates of properties
+        if(req.body.name) 
+            toUpdate.name = req.body.name;
+        if(req.body.color) 
+            toUpdate.color = req.body.color;
+        if(req.body.marketPrice) 
+            toUpdate.marketPrice = req.body.marketPrice;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}failed`);
+    }
 };
