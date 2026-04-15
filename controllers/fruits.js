aa27a8b65ca20b802f40a75a4ea10c82e4bcc70e
Forraclass <S547289@nwmissouri.edu>
2026-04-14 22:53:57 -0500

    Tristen, Implement Parts 5 and 6

--- controllers/fruits.js
+++ controllers/fruits.js
@@ -94,3 +94,31 @@ exports.fruit_view_one_Page = async function(req, res) {
         res.send(`{'error': '${err}'}`);
     }
 };
+
+// Handle building the view for creating a fruit.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.fruit_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+        res.render('fruitcreate', { title: 'Fruit Create'});
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for updating a fruit.
+// query provides the id
+exports.fruit_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+        let result = await Fruit.findById(req.query.id)
+        res.render('fruitUpdate', { title: 'Fruit Update', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/resource.js
+++ routes/resource.js
@@ -28,6 +28,13 @@ router.get('/fruits', fruit_controller.fruit_view_all_Page);
 
 //GET detail fruit page
 router.get('/detail', fruit_controller.fruit_view_one_Page);
+
+// GET create costume page
+router.get('/create', fruit_controller.fruit_create_Page);
+
+/* GET create update page */
+router.get('/update', fruit_controller.fruit_update_Page);
+
 module.exports = router;
 
 
