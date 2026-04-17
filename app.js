59ff2e3ef513395eff2c59068eb8c7737e6551ed
Forraclass <S547289@nwmissouri.edu>
2026-04-17 14:34:01 -0500

    Tristen, Add db connection

--- app.js
+++ app.js
@@ -3,11 +3,40 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user){
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function(err){
+        return done(err)
+    })
+  })
+)
 
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
 mongoose.connect(connectionString);
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function(){
+console.log("Connection to DB succeeded")});
+
 var Fruit = require("./models/fruit");
 
 // We can seed the collection if needed on server start
@@ -51,7 +80,7 @@ async function recreateDB(){
   });
 }
 
-  let reseed = true;
+  let reseed = false;
   if (reseed) {recreateDB();}
 
 
@@ -61,6 +90,7 @@ var quixoticFruitsRouter = require('./routes/quixoticFruit');
 var gridRouter = require('./routes/grid');
 var pickRouter = require('./routes/pick');
 var fruitRouter = require('./routes/resource');
+
 
 var app = express();
 
@@ -72,6 +102,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -81,6 +120,14 @@ app.use('/grid', gridRouter);
 app.use('/pick', pickRouter);
 app.use('/resource', fruitRouter);
 app.use('/fruits', quixoticFruitsRouter);
+
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
